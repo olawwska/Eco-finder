0691de836aea58a900527965419b6cbefa4291c4
olawwska <olwojciech@gmail.com>
2019-08-09 13:29:03 +0200

    modified App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from './Container';
+import GoogleApiComponent
+  from "./Google-based Component/GoogleApiComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MyMap = GoogleApiComponent({
+  apiKey: 'AIzaSyBbllFbOfzQ-i6NZniU8LuJq4LGAR6RwNs '
+})(Container);
+
+export class App extends React.Component {
+
+  render() {
+    return (
+        <MyMap/>
+    );
+  }
 }
 
 export default App;
+
